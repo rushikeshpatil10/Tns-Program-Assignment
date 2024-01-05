ca6a8fc07175dbf353affbaec4ee017fd5af4a82
rushikeshpatil07 <rushikesh.cisapp@gmail.com>
2024-01-05 12:01:03 +0530

    added files of polymorphism

---
+++ Polymorphism/src/com/tnif/CompileTimePolymorphism/Main.java
@@ -0,0 +1,17 @@
+package com.tnif.CompileTimePolymorphism;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println(Addition.add(2,3));
+		System.out.println(Addition.add(2.5f,3));
+		System.out.println(Addition.add(2,3.5f));
+		System.out.println(Addition.add(2.5f,3.5f));
+
+	}
+
+}
---
+++ Polymorphism/src/com/tnif/RunTimePolymorphism/Main.java
@@ -0,0 +1,21 @@
+package com.tnif.RunTimePolymorphism;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bank bk = new Bank();
+		
+        bk = new ICICI();
+        System.out.println("Rate of Interest at ICICI "+ bk.getRateOfInterest());
+        
+        bk = new HDFC();
+        System.out.println("Rate of Interest at HDFC "+ bk.getRateOfInterest());
+        
+        bk = new SBI();
+        System.out.println("Rate of Interest at SBI "+ bk.getRateOfInterest());
+        
+	}
+
+}
